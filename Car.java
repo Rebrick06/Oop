e0c6f46ae956dc7fc2cea18a9d51e2f7ae7f7338
rebgul <rebgul@chalmers.se>
2026-01-28 14:54:41 +0100

    direction igen

--- Car.java
+++ Car.java
@@ -6,12 +6,20 @@ public abstract class Car implements Moveable {
     protected double enginePower; // Engine power of the car
     protected double currentSpeed; // The current speed of the car
     protected Color color; // Color of the car
-    public String modelName; // The car model name
+    public String modelName; // The car model 
+    protected double y;
+    protected double x;
+    protected Direction direction;
+
 
     public Car(int nrDoors, Color color, double enginePower, String modelName){
         this.nrDoors = nrDoors;
         this.color = color;
         this.enginePower = enginePower;
+        this.modelName = modelName;
+        this.y = 0;
+        this.x = 0;
+        this.direction = Direction.NORTH;
     }
 
     public int getNrDoors(){
@@ -63,20 +71,44 @@ public abstract class Car implements Moveable {
 
     @Override
     public void move() {
-        getCurrentSpeed();
+        switch (direction) {
+            case Direction.NORTH ->
+                y += getCurrentSpeed();
+            case Direction.SOUTH ->
+                y -= getCurrentSpeed();
+            case Direction.EAST ->
+                x += getCurrentSpeed();
+            case Direction.WEST ->
+                x -= getCurrentSpeed();
+        }
     }
 
     @Override
     public void turnLeft() {
-        
+        switch (direction) {
+            case Direction.NORTH -> 
+                direction = Direction.WEST;
+            case Direction.WEST ->
+                direction = Direction.SOUTH;
+            case Direction.SOUTH ->
+                direction = Direction.EAST;
+            case Direction.EAST ->
+                direction = Direction.NORTH;
+        }
     }
 
     @Override
     public void turnRight() {
-        throw new UnsupportedOperationException("Not supported yet.");
+         switch (direction) {
+            case Direction.NORTH ->
+                direction = Direction.EAST;
+            case Direction.EAST ->
+                direction = Direction.SOUTH;
+            case Direction.SOUTH ->
+                direction = Direction.WEST;
+            case Direction.WEST -> 
+                direction = Direction.NORTH;
+        }
     }
-
-    
-
-    
 }
+
